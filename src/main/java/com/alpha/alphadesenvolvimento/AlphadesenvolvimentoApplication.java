c129b41154fbe3000c878dfbbd91b959a39b75d0
Isaac Rocha <isaac.ziroldo@gmail.com>
2020-09-01 10:40:08 -0300

    Project Created

---
+++ src/main/java/com/alpha/alphadesenvolvimento/AlphadesenvolvimentoApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.alphadesenvolvimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphadesenvolvimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphadesenvolvimentoApplication.class, args);
+	}
+
+}
