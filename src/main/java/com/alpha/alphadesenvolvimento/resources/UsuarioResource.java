9c6479b380a7cb15d3dead1df958893d474f8c12
Isaac Rocha <isaac.ziroldo@gmail.com>
2020-09-04 14:03:24 -0300

    teste2

--- src/main/java/com/alpha/alphadesenvolvimento/resources/UsuarioResource.java
+++ src/main/java/com/alpha/alphadesenvolvimento/resources/UsuarioResource.java
@@ -3,7 +3,7 @@ package com.alpha.alphadesenvolvimento.resources;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +14,7 @@ import com.alpha.alphadesenvolvimento.entities.Usuario;
 @RequestMapping(value = "api/user")
 public class UsuarioResource {
 
-	@GetMapping
+	@PostMapping
 	public Usuario obterUsuario(@RequestParam(name = "email") String email,
 			@RequestParam(name = "password") String password) {
 		Usuario obj = new Usuario();
